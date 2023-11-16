d999dc12eb2a17f97232bc1cbd799b9c769cfaae
Andrii Maherovskyi <a.maherovskyi@abtollc.com>
2023-11-16 11:34:36 +0200

    initial

---
+++ TestAppCenter/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace TestAppCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            AppCenter.Start("android=c3098eb0-98c5-4d0f-9059-7edcf7f81332;", typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
---
+++ TestAppCenter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAppCenter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
